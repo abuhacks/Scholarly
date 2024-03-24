6d3f1ae4d106fa43e06089e832b15451ac725842
Andrew Choe <110001546+andychoe03@users.noreply.github.com>
2024-03-24 01:53:18 -0400

    Push

--- scholarly/src/app/api/video.js
+++ scholarly/src/app/api/video.js
@@ -1,13 +1,13 @@
 import axios from "axios"
 
-const KEY = "AIzaSyDedKgmKoeNDAr4D6rwLCcuUoymkHs8v40"
+const KEY = "AIzaSyAKao7M_DCnHQ725tegcSHvFTi1SIoDRFI"
 
 export default axios.create({
     baseURL:'https://www.googleapis.com/youtube/v3',
     params:{
         part:'snippet',
         maxResults:5,
-        key:KEY,
+        key:KEY
     },
-    headers:{},
-});
+    headers:{}
+})
